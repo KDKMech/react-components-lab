be53212ae0eb0cd34503d0009e3456d643616857
milestarricone <milestarricone2@gmail.com>
2024-12-04 10:48:42 -0500

    MVP met, Final commit. would like to go back and learn.

---
+++ src/WeatherForcast.jsx
@@ -0,0 +1,20 @@
+import './WeatherForcast.css';
+
+const WeatherForecasts = (props) => {
+  return (
+    <>
+      <div className="weather">
+        <h2>{props.day}</h2>
+        <img src="" alt="" />
+        <p>
+          <span>conditions: </span>{props.conditions}
+        </p>
+        <p>
+          <span>time: </span>{props.time}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default WeatherForecasts
